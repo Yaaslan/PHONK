0683ac5e4b2c9d6cef442c2d747bc7ddcab8987f
Yaaslan <yusufali2900@gmail.com>
2026-04-05 22:21:08 +0300

    Update Dropbox URLs to use direct links

--- run_stream.sh
+++ run_stream.sh
@@ -7,10 +7,10 @@ BANNER_PATH="reklam.png"
 
 # Senin Dropbox Linklerin (dl=1 olarak güncellendi)
 URLS=(
-  "https://www.dropbox.com/scl/fi/eqm0hkcpw8jncseuv4cto/Video.Guru_20260405_013006229.mp4?rlkey=r9d4co9rxow4rjv5jit1yt80a&st=soxi4t4z&dl=1"
-  "https://www.dropbox.com/scl/fi/b435jh25b1gtd5q5cjjak/Video.Guru_20260405_013503897.mp4?rlkey=m1sy3trneot5cw0qjujmmga2a&st=wqy6ixu4&dl=1"
-  "https://www.dropbox.com/scl/fi/6e1zbol07nc2rczqih1qw/Video.Guru_20260405_165947566.mp4?rlkey=lc632r4pe7f8zukk3u2szyb8o&st=bh8ff08w&dl=1"
-  "https://www.dropbox.com/scl/fi/37m7kces06fpmf7sth9w1/Video.Guru_20260405_165605915.mp4?rlkey=8jy8mjim6oaqnmivavtsgq1s0&st=028kzsti&dl=1"
+  "https://dl.dropboxusercontent.com/scl/fi/eqm0hkcpw8jncseuv4cto/Video.Guru_20260405_013006229.mp4?rlkey=r9d4co9rxow4rjv5jit1yt80a&st=soxi4t4z&dl=1"
+  "https://dl.dropboxusercontent.com/scl/fi/b435jh25b1gtd5q5cjjak/Video.Guru_20260405_013503897.mp4?rlkey=m1sy3trneot5cw0qjujmmga2a&st=wqy6ixu4&dl=1"
+  "https://dl.dropboxusercontent.com/scl/fi/6e1zbol07nc2rczqih1qw/Video.Guru_20260405_165947566.mp4?rlkey=lc632r4pe7f8zukk3u2szyb8o&st=bh8ff08w&dl=1"
+  "https://dl.dropboxusercontent.com/scl/fi/37m7kces06fpmf7sth9w1/Video.Guru_20260405_165605915.mp4?rlkey=8jy8mjim6oaqnmivavtsgq1s0&st=028kzsti&dl=1"
 )
 
 while true; do
